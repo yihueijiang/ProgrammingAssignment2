87a23c1edd1c12975ce6b0a980692c9f7446d6a7
yihueijiang <jiangyihuei@gmail.com>
2018-03-12 00:33:22 -0700

    descriptive cache matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+##The initial value of the inverse matrix
+        m <- NULL
+        
+        ##Set the value of the inverse property
+        set <- function(matrix) {
+                x <<-matrix
+                m <<- NULL
+        }
+        
+        ##Get the value of x
+        get <- function(){
+        	x
+        } 
+        
+        ##Set the inverse of matrix
+        setInverse <- function(Inverse){
+         	m <<- Inverse
+         }
+         
+        ##Get the inverse of the matrix
+        getInverse <- function() {
+        	m
+ 	}
+        
+        ##Return the list of functions
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)}
 
 }
 
